4383baa6ab777a1e17ed8b3c13096eb193e686e7
MI-Israr <m.israr1228@gmail.com>
2025-11-18 00:37:11 +0500

    implemented register functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,10 +7,68 @@ export const generateToken = (userId) => {
     expiresIn: "7d",
   });
 };
-export const registerUser = async (req, res) => {};
+export const registerUser = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
 
-export const loginUser = async (req, res) => {};
+export const loginUser = async (req, res) => {
+  try {
+    const { name, email, password, profileImageUrl, adminInviteToken } =
+      req.body;
 
-export const getUserProfile = async (req, res) => {};
+    const userExists = await User.findOne({ email });
 
-export const updateUserProfile = async (req, res) => {};
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    //Determine user role: Admin if correct token is provided, otherwise Member
+    let role = "member";
+    if (
+      adminInviteToken &&
+      adminInviteToken === process.env.ADMIN_INVITE_TOKEN
+    ) {
+      role = "admin";
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    //create new user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      profileImageUrl,
+      role,
+    });
+    //return user with Jwt
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      profileImageUrl: user.profileImageUrl,
+      role: user.role,
+      token: generateToken(user._id),
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
+
+export const getUserProfile = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
+
+export const updateUserProfile = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
