0aaf16e9d6ab521bd263fdab9ac947f938f7723a
Muhammad  Israr <124520126+MI-Israr@users.noreply.github.com>
2026-02-06 00:58:13 +0500

    route bug fixed

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ import cors from "cors";
 import router from "./routes/index.js";
 import dotenv from "dotenv";
 import connectDB from "./config/database.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
 dotenv.config();
+
+// 2. Reconstruct __dirname for ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
